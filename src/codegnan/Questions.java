4df1702561a9c33d7996abf189b82b8530b5bc0c
P Praveen <162541077+Praveen868805@users.noreply.github.com>
2024-10-12 14:19:20 +0530

    Add files via upload
    QUIZ APPLICATIONS

---
+++ src/codegnan/Questions.java
@@ -0,0 +1,22 @@
+package codegnan;
+
+public interface Questions {
+	String question1="who is the father of java?";
+	String question2="who is the captain of indian cricket team";
+	String question3="who is the captain of SRH";
+	String question4="What year did the Titanic sink?";
+	String question5="What is the longest river in the world?";
+	String question6="What is the chemical symbol for iron?";
+	String question7="What is the chemical formula for water?";
+	String question8="What is the chemical symbol for gold?";
+	String question9="What is the largest ocean in the world?";
+	String question10="Which planet is known as the Red Planet?";
+	String question11="Who is the captain of MI";
+	
+	
+	
+	
+	
+	
+
+}

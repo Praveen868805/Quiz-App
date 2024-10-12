995791dec263cabf680250356ec01685b205991a
P Praveen <162541077+Praveen868805@users.noreply.github.com>
2024-10-12 14:14:26 +0530

    Delete QuizApplication directory

--- QuizApplication/src/codegnan/Guidelines.java
+++
@@ -1,8 +0,0 @@
-package codegnan;
-
-public interface Guidelines {
-	String guideline1= "1.There will be 10 questions in the game";
-	String guideline2="2.In each question having reward amount";
-	String guideline3="3.correct question reward amount 1000rs";
-
-}
